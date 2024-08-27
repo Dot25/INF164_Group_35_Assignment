3ffa8637d3c170c82ccf08effeb74cd214f89a4b
Mulondi Makhado <makhadomulondi@gmail.com>
2024-08-27 14:02:20 +0200

    First Commit

--- Group_Project/Form1.cs
+++ Group_Project/Form1.cs
@@ -11,10 +11,16 @@ using System.Windows.Forms;
 namespace Group_Project
 {
     public partial class Form1 : Form
-    {
+    { 
         public Form1()
         {
             InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
