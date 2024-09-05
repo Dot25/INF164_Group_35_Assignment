535fb2ddfd8c5743669bf9d43797491550918c33
Mulondi Makhado <makhadomulondi@gmail.com>
2024-09-05 10:35:31 +0200

    Temporarly added the images for the MainGameForm and HighScoreForm

--- Group_Project/MainGameForm.Designer.cs
+++ Group_Project/MainGameForm.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainGameForm));
             this.imageListBackground = new System.Windows.Forms.ImageList(this.components);
             this.lblWord = new System.Windows.Forms.Label();
             this.btnMine = new System.Windows.Forms.Button();
@@ -96,6 +97,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.pbxAxe);
             this.Controls.Add(this.pbxOre);
