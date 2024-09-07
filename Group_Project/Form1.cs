ef765d1d11e9790956ca84dd60dccd0df7744ec2
Brandon Piel <brandonanthonypiel@gmail.com>
2024-09-07 11:23:17 +0200

    Music plays when you press the music button

--- Group_Project/Form1.cs
+++ Group_Project/Form1.cs
@@ -15,15 +15,12 @@ namespace Group_Project
 {
     public partial class pnlWelcomeForm : Form
     {
-        SoundPlayer music;
+        
         public pnlWelcomeForm()
         {
             InitializeComponent();
 
-            //music = new SoundPlayer(@"Resources/fellowship.wav");
-            //string exePath = Path.GetDirectoryName(Application.ExecutablePath);
-
-            // Combine it with the relative path to the Resources folder and the .wav file
+            
             
             
 
@@ -48,20 +45,9 @@ namespace Group_Project
 
         private void btnMusic_Click(object sender, EventArgs e)
         {
-            string wavFilePath = Path.Combine("Resources", "fellowship.wav");
-            if (File.Exists(wavFilePath))
-            {
-                music = new SoundPlayer(wavFilePath);
-                MessageBox.Show("File found!");
-            }
-            else
-            {
-                MessageBox.Show("WAV file not found!");
-            }
-            //music.PlayLooping();
-
-            //music.PlayLooping(); // Plays the sound once
-            // player.PlayLooping(); // Uncomment this line to loop the sound
+            SoundPlayer audio = new SoundPlayer(Group_Project.Properties.Resources.fellowship); 
+            audio.PlayLooping();
+            
 
         }
 
